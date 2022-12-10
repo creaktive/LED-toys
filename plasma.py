9e1102a42daad644f3a2bff47e520bb485340eb0
Stanislaw Pusep <creaktive@gmail.com>
2022-12-10 17:24:26 +0100

    CLI

--- plasma.py
+++ plasma.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+import sys
+from argparse import ArgumentParser
 from math import pi, sin
 from noise import snoise2
 from rpi_ws281x import PixelStrip, Color
@@ -12,35 +15,38 @@ def rainbow(p):
     )
 
 if __name__ == '__main__':
-    fps = 30
-    gpio = 12
-    brightness = 16
-    width = 128
-
-    octaves = 4
-    freq = 16.0 * octaves
+    parser = ArgumentParser(description='Pipe pixel colors to a LED strip')
+    parser.add_argument('--brightness', default=255, type=int, help='set to 0 for darkest and 255 for brightest (default: 255)')
+    parser.add_argument('--fps', default=30, type=int, help='aim for specified frames per second (default: 30)')
+    parser.add_argument('--gpio', default=12, type=int, help='GPIO pin connected to the LED strip (default: 12)')
+    parser.add_argument('--leds', default=128, type=int, help='how many LEDs to light up (default: 128)')
+    parser.add_argument('--octaves', default=2, type=int, help='noise octaves (default: 2)')
+    args = parser.parse_args()
+    sys.argv = []
 
     strip = PixelStrip(
-        width,
-        gpio,
-        brightness=brightness,
+        args.leds,
+        args.gpio,
+        brightness=args.brightness,
     )
-    strip.begin()
 
     try:
+        freq = 16.0 * args.octaves
         y = 0
+
+        strip.begin()
         while True:
-            for x in range(width):
-                color = rainbow(snoise2(x / freq, y / freq, octaves))
+            for x in range(args.leds):
+                color = rainbow(snoise2(x / freq, y / freq, args.octaves))
                 strip.setPixelColor(x, color)
             strip.show()
 
-            sleep(1 / fps)
+            sleep(1 / args.fps)
             y += 1
     except KeyboardInterrupt:
         print('')
     finally:
         black = Color(0, 0, 0)
-        for x in range(width):
+        for x in range(args.leds):
             strip.setPixelColor(x, black)
         strip.show()
