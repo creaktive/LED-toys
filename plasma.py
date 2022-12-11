c266557b76d8e2e0bfaa71467bf536f45af6e3f2
Stanislaw Pusep <creaktive@gmail.com>
2022-12-11 11:37:24 +0100

    smoother but slower

--- plasma.py
+++ plasma.py
@@ -64,13 +64,14 @@ def sleep_ns(ns):
 if __name__ == '__main__':
     parser = ArgumentParser(description='Play plasma effect on a WS2812B RGB LED strip')
     parser.add_argument('--brightness', default=255, type=int, help='set to 0 for darkest and 255 for brightest (default: 255)')
-    parser.add_argument('--fps', default=30, type=int, help='aim for specified frames per second (default: 30)')
+    parser.add_argument('--fps', default=48, type=int, help='aim for specified frames per second (default: 48)')
     parser.add_argument('--gpio', default=12, type=int, help='GPIO pin connected to the LED strip (default: 12)')
     parser.add_argument('--leds', default=288, type=int, help='how many LEDs to light up (default: 288)')
     parser.add_argument('--octaves', default=5, type=int, help='noise octaves (default: 5)')
     parser.add_argument('--sway_amount', default=100.0, type=float, help='sway amount (default: 100.0)')
     parser.add_argument('--sway_period', default=30.0, type=float, help='sway period, in seconds (default: 30.0)')
     parser.add_argument('--gamma', default=0.8, type=float, help='gamma (default: 0.8)')
+    parser.add_argument('--scroll_step', default=0.1, type=float, help='scroll step (default: 0.1)')
     args = parser.parse_args()
 
     strip = PixelStrip(
@@ -81,7 +82,7 @@ if __name__ == '__main__':
 
     freq = 16.0 * args.octaves
     interval = int(NANOSECONDS_IN_SECOND / args.fps)
-    y = 0
+    y = 0.0
 
     killer = GracefulKiller()
     strip.begin()
@@ -93,7 +94,7 @@ if __name__ == '__main__':
             noise = clamp(0.5 + snoise2((x + sway) / freq, y / freq, args.octaves))
             color = convert_wave_length_nm_to_rgb(380.0 + 400.0 * noise, args.gamma)
             strip.setPixelColor(x, color)
-        y += 1
+        y += args.scroll_step
         strip.show()
 
         now = monotonic_ns()
